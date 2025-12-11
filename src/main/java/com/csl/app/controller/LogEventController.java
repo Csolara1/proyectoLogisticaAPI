e22e982aca894edfafc91ecff1511751d97b9348
Csolara1 <rubiker83@gmail.com>
2025-12-11 11:45:55 +0100

    segundo commit

--- src/main/java/com/csl/app/controller/LogEventController.java
+++ src/main/java/com/csl/app/controller/LogEventController.java
@@ -3,6 +3,7 @@ package com.csl.app.controller;
 import com.csl.app.model.LogEvent;
 import com.csl.app.repository.LogEventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -33,4 +34,11 @@ public class LogEventController {
         log.setSourceModule("FRONTEND");
         logRepository.save(log);
     }
+
+    @DeleteMapping("/logs")
+public ResponseEntity<String> vaciarLogs() {
+    // Asumiendo que usas un repositorio llamado logRepository
+    logRepository.deleteAll(); 
+    return ResponseEntity.ok("Registro de eventos vaciado correctamente.");
+}
 }
